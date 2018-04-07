2eb0753d3e7cc554959b80c3058092c99ad8c9a8
VitaYoung <690326264@qq.com>
2018-04-07 20:47:51 +0800

    vue-cli version
    用vue-cli 重构了之前的代码

---
+++ Vue-cli-version/merchant-end/src/components/Content1.vue
@@ -0,0 +1,10 @@
+<template>
+	<Content :style="{padding: '0 16px 16px'}">
+  	<Breadcrumb :style="{margin: '16px 0'}">
+    	<BreadcrumbItem>Home</BreadcrumbItem>
+    </Breadcrumb>
+  	<Card>
+      <div style="height: 1000px">Content</div>
+  	</Card>
+  </Content>
+</template>
---
+++ Vue-cli-version/merchant-end/src/components/Dish.vue
@@ -0,0 +1,21 @@
+<style scoped>
+	.dishes {
+			margin: 5%;
+			width: 80%;
+}
+</style>
+
+<template>
+	<Card :bordered="false" class="dishes">
+		<p slot="title">{{dishname}}</p>
+		<p>{{description}}</p>
+		<br></br>
+		<Button v-on:click="$emit('remove')">delete</Button>
+	</Card>
+</template>
+
+<script>
+	export default {
+		props: ['dishname', 'description']
+	}
+</script>
